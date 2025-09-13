6c10ebafbdd04de9270ff777c6449c5980a4e763
natisis0 <natnaesisay4@gmail.com>
2025-09-13 17:38:11 +0300

    first project using public api

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://v2.jokeapi.dev/joke/Any?type=single"
+    );
+    // console.log(result.data.joke)
+    res.render("index.ejs", {
+      joke: result.data.joke,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
